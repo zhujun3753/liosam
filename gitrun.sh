b3b3a0f4aa93127b870c2fb1c4de230a01b5857e
zhu jun <zhujun3753@163.com>
2021-11-13 15:28:42 +0800

    change

--- gitrun.sh
+++ gitrun.sh
@@ -1,6 +1,6 @@
 git status
 git add .
 git commit -m 'change'
-# git branch -M main
-# git remote add origin https://github.com/zhujun3753/liosam.git
+git branch -M main
+git remote add origin https://github.com/zhujun3753/liosam.git
 git push -u origin main
